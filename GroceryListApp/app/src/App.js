9ed7e4b467ef5effc6e09c69c17b44048d58133c
Kelly Jiang <kellyjiang3773@gmail.com>
2023-07-01 16:10:22 -0400

    Add create/delete functionality on UI

--- GroceryListApp/app/src/App.js
+++ GroceryListApp/app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery, useMutation } from '@apollo/client';
 
 
 const client = new ApolloClient({
@@ -17,23 +17,80 @@ const QUERY_GROCERY_ITEMS = gql`
   }
 `;
 
+const CREATE_GROCERY_ITEM = gql`
+  mutation createGroceryItem ($itemName: String!) {
+    createGroceryItem(itemName: $itemName) {
+      item {
+        id
+        itemName
+        purchased
+      }
+    }
+  }
+`;
+
+const DELETE_GROCERY_ITEM = gql`
+  mutation deleteGroceryItem ($id: ID!) {
+    deleteGroceryItem(id: $id) {
+      ok
+    }
+  }
+`;
+
 client.query({ query: QUERY_GROCERY_ITEMS }).then((result) => console.log(result));
 
+const CreateItemInput = () => {
+  let itemName;
+  const [createGroceryItem] = useMutation(CREATE_GROCERY_ITEM);
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          createGroceryItem({ variables: {
+            itemName: itemName.value,
+        } });
+        itemName.value = '';
+        window.location.reload();
+      }}
+      style = {{ marginTop: '2em', marginBottom: '2em' }}
+     >
+     <label>New item: </label>
+     <input
+       ref={node => {
+        itemName = node;
+       }}
+       style={{ marginRight: '1em' }}
+     />
+     <button type="submit" style={{ cursor: 'pointer' }}>Add</button>
+    </form>
+   </div>
+  );
+}
+
 const ItemList = () => {
+  const [deleteGroceryItem] = useMutation(DELETE_GROCERY_ITEM);
   const { loading, error, data } = useQuery(
     QUERY_GROCERY_ITEMS,
-    {
-      pollInterval: 2000
-    }
+    // { pollInterval: 2000 }
   );
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   return data.groceryItems.map(( {id, itemName, purchased }) => (
-    <div key={id}>
-      <h3>{itemName}</h3>
-      {purchased ? <p>(purchased)</p> : <p>(not purchased yet)</p>}
+    <div 
+      key={id} 
+      style={{
+        display: 'flex',
+        justifyContent:'left',
+        alignItems:'left',
+        flexDirection: 'row',
+    }}>
+      <button onClick={() => {
+        deleteGroceryItem({ variables: {id: id} });
+        window.location.reload();
+      }}>X</button>
+      <p>{itemName} - {purchased ? "(purchased)" : "(not purchased yet)"}</p>
     </div>
   ))
 }
@@ -43,11 +100,12 @@ const App = () => (
     <div style={{
       backgroundColor: '#00000008',
       display: 'flex',
-      justifyContent:'center',
-      alignItems:'center',
+      justifyContent:'left',
+      alignItems:'left',
       height: '100vh',
       flexDirection: 'column',
     }}>
+      <CreateItemInput />
       <ItemList />
     </div>
   </ApolloProvider>
